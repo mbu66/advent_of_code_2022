c9a7e4244b738541685bb0cea667e21a6caf7781
mbu66 <mbk@greenhills.co.uk>
2022-12-08 00:18:52 +0000

    Suppress dead_code warning

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -4,6 +4,7 @@ use std::{
     path::Path,
 };
 
+#[allow(dead_code)]
 pub fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
     BufReader::new(File::open(filename)?).lines().collect()
 }
