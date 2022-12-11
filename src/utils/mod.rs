76465944183df7aa049873f016eb95ab3048c5c6
mbu66 <mbk@greenhills.co.uk>
2022-12-11 17:03:00 +0000

    Added function timing

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -8,3 +8,11 @@ use std::{
 pub fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
     BufReader::new(File::open(filename)?).lines().collect()
 }
+
+#[allow(dead_code)]
+pub fn chrono<F: Fn() -> T, T>(f: F) -> T {
+    let now = std::time::Instant::now();
+    let result = f();
+    print!("Time Taken = {}ms\n", now.elapsed().as_millis());
+    result
+  }
