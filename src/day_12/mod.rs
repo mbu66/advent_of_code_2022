cf5a5c55d18e061d7dd3d5cf0980bfb11794ce88
mbu66 <mbk@greenhills.co.uk>
2022-12-12 21:29:52 +0000

    Completed Day 12

--- src/day_12/mod.rs
+++ src/day_12/mod.rs
@@ -10,20 +10,21 @@ fn propagate<const UP: bool >(topology: &Vec<Vec<i32>>, coord_a: (usize, usize),
     }
 }
 
+fn parse_coord<const UP: bool >(topology: &Vec<Vec<i32>>, start: (usize, usize), coord: (usize, usize), active_coords: &mut Vec<(usize, usize)>, distances: &mut Vec<Vec<i32>>) {
+    if coord.0 != distances.len() - 1 { propagate::<UP>(&topology, coord, (coord.0 + 1, coord.1), active_coords, distances); }
+    if coord.1 != distances[0].len() - 1 { propagate::<UP>(&topology, coord, (coord.0, coord.1 + 1), active_coords, distances); }
+    if coord.0 != 0 { propagate::<UP>(&topology, coord, (coord.0 - 1, coord.1), active_coords, distances); }
+    if coord.1 != 0 { propagate::<UP>(&topology, coord, (coord.0, coord.1 - 1), active_coords, distances); }
+    if coord == start { distances[start.0][start.1] = 1; }
+}
+
 fn parse_topology_down(topology: &Vec<Vec<i32>>, start: (usize, usize),) -> i32 {
-    let mut distances = vec![vec![0; topology[0].len()]; topology.len()];
-    let mut active_coords = vec![start];
-    let mut end_reached = false;
-    let mut end = (0, 0);
+    let ( mut distances, mut active_coords, mut end_reached, mut end) = (vec![vec![0; topology[0].len()]; topology.len()], vec![start], false, (0, 0));
     while end_reached == false {
         let local_coords = active_coords.clone();
         active_coords.clear();
         for coord in local_coords {
-            if coord.0 != distances.len() - 1 { propagate::<false>(&topology, coord, (coord.0 + 1, coord.1), &mut active_coords, &mut distances); }
-            if coord.1 != distances[0].len() - 1 { propagate::<false>(&topology, coord, (coord.0, coord.1 + 1), &mut active_coords, &mut distances); }
-            if coord.0 != 0 { propagate::<false>(&topology, coord, (coord.0 - 1, coord.1), &mut active_coords, &mut distances); }
-            if coord.1 != 0 { propagate::<false>(&topology, coord, (coord.0, coord.1 - 1), &mut active_coords, &mut distances); }
-            if coord == start { distances[start.0][start.1] = 1; }
+            parse_coord::<false>(&topology, start, coord, &mut active_coords, &mut distances);
             if topology[coord.0][coord.1] == 0 { end_reached = true; end = coord.clone(); }
         }
     }
@@ -31,18 +32,12 @@ fn parse_topology_down(topology: &Vec<Vec<i32>>, start: (usize, usize),) -> i32 
 }
 
 fn parse_topology_up(topology: &Vec<Vec<i32>>, start: (usize, usize), end: (usize, usize)) -> i32 {
-    let mut distances = vec![vec![0; topology[0].len()]; topology.len()];
-    let mut active_coords = vec![start];
-    let mut end_reached = false;
+    let (mut distances, mut active_coords, mut end_reached) = (vec![vec![0; topology[0].len()]; topology.len()], vec![start], false);
     while end_reached == false {
         let local_coords = active_coords.clone();
         active_coords.clear();
         for coord in local_coords {
-            if coord.0 != distances.len() - 1 { propagate::<true>(&topology, coord, (coord.0 + 1, coord.1), &mut active_coords, &mut distances); }
-            if coord.1 != distances[0].len() - 1 { propagate::<true>(&topology, coord, (coord.0, coord.1 + 1), &mut active_coords, &mut distances); }
-            if coord.0 != 0 { propagate::<true>(&topology, coord, (coord.0 - 1, coord.1), &mut active_coords, &mut distances); }
-            if coord.1 != 0 { propagate::<true>(&topology, coord, (coord.0, coord.1 - 1), &mut active_coords, &mut distances); }
-            if coord == start { distances[start.0][start.1] = 1; }
+            parse_coord::<true>(&topology, start, coord, &mut active_coords, &mut distances);
             if coord == end { end_reached = true; }
         }
     }
