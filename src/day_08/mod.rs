5b9e215f792b5ef8173e05b1a4cf265654ec7bff
mbu66 <mbk@greenhills.co.uk>
2022-12-21 23:06:45 +0000

    Refactored Day 08 to improve performance

--- src/day_08/mod.rs
+++ src/day_08/mod.rs
@@ -1,37 +1,29 @@
 #[path = "../utils/mod.rs"] mod utils;
 
-fn parse_forrest(forrest_in: &Vec<String>, size: usize) -> (u32, u32) {
-    
-    let mut forrest = vec![vec![0; size]; size];
-    let mut forrest_t = vec![vec![0; size]; size];
-    for i in 0..size {
+fn parse_forrest(forrest_in: &Vec<String>) -> (u32, u32) {
+    let mut visible_trees = 0;
+    let mut max_scenic_score = 0;
+    let mut forrest = vec![vec![0; forrest_in.len()]; forrest_in.len()];
+    let mut forrest_t = vec![vec![0; forrest_in.len()]; forrest_in.len()];
+    for i in 0..forrest_in.len() {
         for (j, tree) in forrest_in[i].chars().enumerate() {
             forrest[i][j] = tree.to_digit(10).unwrap();
             forrest_t[j][i] = tree.to_digit(10).unwrap();
         }
     }
-
-    let mut visible_trees = 0;
-    let mut max_scenic_score = 0;
-
     for i in 0..forrest.len() {
         for j in 0..forrest.len() {
-
             let tree_height = forrest[i][j];
-
             let mut trees_l = forrest[i][..j].to_vec();
             let mut trees_t = forrest_t[j][..i].to_vec();
             let trees_r = forrest[i][(j+1)..].to_vec();
             let trees_b = forrest_t[j][(i+1)..].to_vec();
             trees_l.reverse();
             trees_t.reverse();
-            
             let scenic_score = scenic_score(&trees_l, &tree_height) * scenic_score(&trees_t, &tree_height) * 
                                scenic_score(&trees_r, &tree_height) * scenic_score(&trees_b, &tree_height);
-
             let is_visible = is_visible(&trees_l, &tree_height) || is_visible(&trees_t, &tree_height) || 
                              is_visible(&trees_r, &tree_height) || is_visible(&trees_b, &tree_height);
-            
             if max_scenic_score < scenic_score { max_scenic_score = scenic_score}
             if is_visible { visible_trees += 1; }
         }
@@ -51,7 +43,7 @@ fn scenic_score(trees: &Vec<u32>, height: &u32) -> u32{
 
 pub fn run() {
     let lines = utils::lines_from_file("./src/day_08/input.txt").expect("Failed to read line from file");
-    let (num_visible_trees, max_scenic_score) = parse_forrest(&lines, 99);
+    let (num_visible_trees, max_scenic_score) = parse_forrest(&lines);
     assert_eq!(num_visible_trees, 1851);
     assert_eq!(max_scenic_score, 574080);
 }
